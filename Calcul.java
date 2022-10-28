eade0870b0bdab63e65e939c9b1e16fa838fb365
AzizAbd <91793452+AzizAbd@users.noreply.github.com>
2022-10-28 21:59:22 +0200

    first commit

---
+++ Calcul.java
@@ -0,0 +1,17 @@
+public class Calcul 
+{
+	private double resultat;
+	
+	public Calcul()                    { this.resultat = 0.0;  }
+	
+	public void reset()                { this.resultat = 0.0;  }
+	
+	public void addition    (double x, double y)   { this.resultat = x + y;     }
+	
+	public void soustraction(double x, double y)   { this.resultat = x - y;     }
+	
+	public void multiplication(double x, double y) { this.resultat = y * x; 	}
+	
+	public String getResultat()   { return Double.toString(resultat); }
+	
+}
